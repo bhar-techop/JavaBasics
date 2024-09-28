f1f6d6fd8ab0fc962db3f2e1523a883d61c5c844
bhar-techop <bhargavprabhu98@gmail.com>
2024-09-28 19:35:26 +0530

    Typecasting is completed

---
+++ Fundamentals/src/in/basics/Explicit_TypeCasting.java
@@ -0,0 +1,11 @@
+package in.basics;
+
+public class Explicit_TypeCasting{
+  
+	public static void main(String[] args) {
+		int a=100;
+		byte b=(byte)a;
+		System.out.println(b);
+	}
+
+}
---
+++ Fundamentals/src/in/basics/Implicit_TypeCasting.java
@@ -0,0 +1,12 @@
+package in.basics;
+
+public class Implicit_TypeCasting {
+public static void main(String[]args) {
+	byte b=100;
+//	byte b=156;//it show error because byte range -128 to 127
+	int c=b;
+	
+			System.out.println(b);
+			System.out.println(c);
+}
+}
