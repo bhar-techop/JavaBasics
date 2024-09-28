ec040adfed892ef5e8f9e2d6c827221f6a209a83
bhar-techop <bhargavprabhu98@gmail.com>
2024-09-28 22:05:07 +0530

    method  is completed

---
+++ Fundamentals/src/in/basics/Method_I.java
@@ -0,0 +1,15 @@
+package in.basics;
+
+public class Method_I {
+	//function definition
+      public void m1() {
+    	  System.out.println("hello");
+      }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Method_I obj=new Method_I();
+		//calling function:objname.methodname()
+		obj.m1();
+	}
+
+}
