465a2173366ca99d47de60aa5a472e99af6fcb24
bhar-techop <bhargavprabhu98@gmail.com>
2024-09-28 11:21:25 +0530

    data type is completed

---
+++ Fundamentals/src/in/basics/Datatype_I.java
@@ -0,0 +1,17 @@
+package in.basics;
+
+public class Datatype_I {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      System.out.println("byte size:" +Byte.SIZE+" bits");
+      System.out.println("short size:" +Short.SIZE+" bits");
+      System.out.println("int size:" +Integer.SIZE+" bits");
+      System.out.println("long size:" +Long.SIZE+" bits");
+      System.out.println("float size:" +Float.SIZE+" bits");
+        System.out.println("double size:" +Double.SIZE+" bits");
+      System.out.println("char size:" +Character.SIZE+" bits");
+   
+	}
+
+}
---
+++ Fundamentals/src/in/basics/Datatype_II.java
@@ -0,0 +1,13 @@
+package in.basics;
+
+public class Datatype_II {
+public static void main(String[] args) {
+	System.out.println("byte range:" +Byte.MIN_VALUE+"-"+Byte.MAX_VALUE);
+    System.out.println("short range:" +Short.MIN_VALUE+"-"+Short.MAX_VALUE);
+    System.out.println("int range:" +Integer.MIN_VALUE+"-"+Integer.MAX_VALUE);
+    System.out.println("long range:" +Long.MIN_VALUE+"-"+Long.MAX_VALUE);
+    System.out.println("float range:" +Float.MIN_VALUE+"-"+Float.MAX_VALUE);
+    System.out.println("double range:" +Double.MIN_VALUE+"-"+Double.MAX_VALUE);
+  
+}
+}
